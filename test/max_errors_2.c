b7970e206659ba3fd66fa3f87160615f072629a5
Yury Gribov <tetra2005@gmail.com>
2015-12-05 15:25:00 +0300

    Added test for SORTCHECK_OPTIONS.

---
+++ test/max_errors_2.c
@@ -0,0 +1,22 @@
+// OPTS: max_errors=1
+
+#include <stdlib.h>
+
+char aa[] = { 1, 2, 3 };
+
+int cmp(const void *pa, const void *pb) {
+  char a = *(const char *)pa;
+  char b = *(const char *)pb;
+  // CHECK: comparison function modifies data
+  int res = a == b ? 1 : 0;
+  // CHECK-NOT: comparison function is not symmetric
+  *(char *)pa = 100;
+}
+
+int main() {
+  qsort(aa, sizeof(aa), 1, cmp);
+  int key = 1;
+  bsearch(&key, aa, sizeof(aa), 1, cmp);
+  return 0;
+}
+
--- test/test.sh
+++ test/test.sh
@@ -9,23 +9,47 @@ error() {
   num_errors=$((num_errors + 1))
 }
 
+has_option() {
+  grep -q "// *$2 *:" $1
+}
+
+get_option() {
+  cat $1 | sed -n 's!.*\/\/ *'$2' *: *!!p'
+}
+
 CHECK_PAT='// *CHECK *:'
+OPTS_PAT='// *OPTS *:'
 
 for t in test/*.c; do
   gcc $t -Itest -o bin/a.out
+  if has_option $t OPTS; then
+    export SORTCHECK_OPTIONS=$(get_option $t OPTS)
+  else
+    unset SORTCHECK_OPTIONS
+  fi
   if ! LD_PRELOAD=bin/libsortcheck.so bin/a.out 2>bin/a.out.log; then
     error "$t: test exited with a non-zero exit code"
-  elif ! grep -q "$CHECK_PAT" $t && test -s bin/a.out.log; then
+  elif ! has_option $t CHECK && ! has_option $t CHECK-NOT && test -s bin/a.out.log; then
     error "$t: non-empty stderr"
   else
-    cat $t | sed -n 's!^\/\/ *CHECK *: *!!p' > bin/checks.txt
     failed=
+
+    get_option $t CHECK > bin/checks.txt
     while read check; do
       if ! grep -q "$check" bin/a.out.log; then
         error "$t: output does not match pattern '$check'"
         failed=1
       fi
     done < bin/checks.txt
+
+    get_option $t CHECK-NOT > bin/checknots.txt
+    while read checknot; do
+      if grep -q "$checknot" bin/a.out.log; then
+        error "$t: output matches prohibited pattern '$check'"
+        failed=1
+      fi
+    done < bin/checknots.txt
+
     if test -z "$failed"; then
       echo "$t: OK"
     fi
