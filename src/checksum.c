1c0c72f3b46952162959c4740da72ec04c4623ed
Yury Gribov <tetra2005@gmail.com>
2015-12-19 20:02:59 +0300

    Respect NULL pointers passed to qsort.

--- src/checksum.c
+++ src/checksum.c
@@ -5,7 +5,7 @@
 unsigned checksum(const void *data, size_t sz) {
   uint16_t s1 = 0, s2 = 0;
   size_t i;
-  for(i = 0; i < sz; ++i) {
+  for(i = 0; data && i < sz; ++i) {
     s1 = (s1 + ((const uint8_t *)data)[i]) % UINT8_MAX;
     s2 = (s2 + s1) % UINT8_MAX;
   }
