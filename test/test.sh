96139ec97f8582c4ff6a6d357477f3d10230e276
Yury Gribov <tetra2005@gmail.com>
2018-10-05 23:42:52 +0100

    Disable syslog tests in Travis environment.

--- test/test.sh
+++ test/test.sh
@@ -25,14 +25,24 @@ get_option() {
 }
 
 get_syslog() {
-  if test -f /var/log/syslog; then
+  if test -n "${TRAVIS:-}"; then
+    :
+  elif test -f /var/log/syslog; then
     cat /var/log/syslog
   else
     journalctl -q
   fi
 }
 
+# From https://github.com/google/sanitizers/wiki/AddressSanitizer
+export ASAN_OPTIONS=strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1
+
+# Get rid of "ASan runtime does not come first in initial library list"
+ASAN_OPTIONS=verify_asan_link_order=0:$ASAN_OPTIONS
+
 for t in test/*.c; do
+  failed=
+
   if ! gcc $t -Itest $(get_option $t CFLAGS) -o bin/a.out; then
     error "$t: compilation failed"
     failed=1
@@ -48,16 +58,20 @@ for t in test/*.c; do
   else
     ARGS=
   fi
+  if has_option $t SYSLOG && test -n "${TRAVIS:-}"; then
+    echo "Skipping syslog test $t in Travis environment"
+    continue
+  fi
 
   get_syslog > bin/syslog.bak
 
   if ! LD_PRELOAD=${LD_PRELOAD:+$LD_PRELOAD:}bin/libsortcheck.so bin/a.out $ARGS 2>bin/a.out.log; then
     error "$t: test exited with a non-zero exit code"
+    failed=1
   elif ! has_option $t CHECK && ! has_option $t CHECK-NOT && test -s bin/a.out.log; then
     error "$t: non-empty stderr"
+    failed=1
   else
-    failed=
-
     get_option $t CHECK > bin/checks.txt
     while read check; do
       if ! grep -q "$check" bin/a.out.log; then
@@ -101,3 +115,4 @@ for t in test/*.c; do
 done
 
 echo "Total: $num_errors errors"
+test $num_errors = 0 || exit 1
