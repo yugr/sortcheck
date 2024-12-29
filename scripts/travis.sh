42a7d1628b89713eb2cbf6fe866bd8148193ac56
Yury Gribov <tetra2005@gmail.com>
2024-12-29 21:02:17 +0300

    Use gmake on BSD.

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright 2019-2021 Yury Gribov
+# Copyright 2019-2024 Yury Gribov
 # 
 # Use of this source code is governed by MIT license that can be
 # found in the LICENSE.txt file.
@@ -15,8 +15,13 @@ cd $(dirname $0)/..
 
 export ASAN_OPTIONS='detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1:strict_string_checks=1'
 
-make "$@" clean all
-make "$@" check
+MAKE=make
+if uname | grep -q BSD; then
+  MAKE=gmake
+fi
+
+$MAKE "$@" clean all
+$MAKE "$@" check
 
 if test -n "${COVERAGE:-}"; then
   # Collect DLL coverage
